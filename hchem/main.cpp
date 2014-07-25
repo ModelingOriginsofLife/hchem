869eb8456fcfdf283488be65b02d6aa8f8656a1f
Matteo Monti <monti@MacBook-Pro-di-Matteo.local>
2014-07-25 11:43:35 +0900

    Initial Commit

---
+++ hchem/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hchem
+//
+//  Created by Matteo Monti on 25/07/14.
+//  Copyright (c) 2014 Modeling Origins of Life. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
